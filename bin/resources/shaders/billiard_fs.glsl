fde57132c67b12370e67bb59a8c0a3c366246ebb
Brave Fang <1796574499@qq.com>
2023-12-22 15:42:23 +0800

    fix

---
+++ bin/resources/shaders/billiard_fs.glsl
@@ -0,0 +1,37 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexCoord;
+
+uniform vec3 lightPos;
+uniform vec3 lightColor;
+
+uniform vec3 cameraPos;
+
+uniform sampler2D diffuse_texture;
+
+void main()
+{
+    // ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor * 0.1;
+
+    // specular
+    float specularStrength = 5;
+    vec3 viewDir = normalize(cameraPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    vec3 objectColor = texture(diffuse_texture, TexCoord).rgb;
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    FragColor = vec4(result, 1.0);
+}
